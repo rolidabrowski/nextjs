ac9bc56194cf7336fd625090c5a8a968563d3387
Roland <roli.dabrowski@gmail.com>
2024-05-03 16:37:48 +0200

    edited schema

--- auth.ts
+++ auth.ts
@@ -2,9 +2,9 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
-import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 import { db } from './app/lib/db';
+import { User } from '@prisma/client';
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
